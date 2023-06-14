f9ca90f4cfbb41f0378a651df90846dc03e35782
rysbeck <rysbeck1@gmail.com>
2023-06-14 05:58:57 +0400

    fetched DB, React skeleton, React Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
+
 import './scss/app.scss';
+
 import Header from './components/Header'
-import Categories from './components/Categories'
-import Sort from './components/Sort'
-import PizzaBlock from './components/PizzaBlock'
-import pizzas from './assets/pizza.json'
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NonFound';
+import {Routes,Route} from "react-router-dom";
+
 
 function App() {
-  const pizzasData = pizzas.pizzas;
   return (
     <div className="wrapper">
      <Header/>
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories/>
-           <Sort/>
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {pizzasData.map((obj)=>(
-              <PizzaBlock key={obj.id} {...obj}/> 
-            ))} 
-          </div>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/cart' element={<Cart/>} />
+            <Route path='*' element={<NotFound/>} />
+          </Routes>
           </div>
         </div>
       </div>
